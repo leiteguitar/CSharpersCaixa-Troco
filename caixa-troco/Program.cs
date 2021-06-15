911d453884f828825a255f41381a698f77aa85d1
Vinny <vih_leite@hotmail.com>
2021-06-15 00:26:31 -0300

    exerciio 1 caixa troco

---
+++ caixa-troco/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace caixa_troco
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string pagamento = "";
+            string precoDoProduto;
+
+            //while ()
+            Console.WriteLine("Digite o preço do produto: ");
+            precoDoProduto = Console.ReadLine();
+            double precoReal = Convert.ToDouble(precoDoProduto);
+
+            Console.WriteLine("Digite o valor do pagamento: ");
+            pagamento = Console.ReadLine();
+            pagamento = pagamento.ToString();
+
+            while (pagamento != "")
+            {
+                double pagamentoReal = Convert.ToDouble(pagamento);
+                double troco = precoReal - pagamentoReal;
+
+                if (troco == 0)
+                    Console.WriteLine("Não precisa de troco");
+                else
+                    Console.WriteLine("Seu troco é de " + troco);
+
+                Console.WriteLine("Digite o valor do pagamento: ");
+                pagamento = Console.ReadLine();
+                pagamento = pagamento.ToString();
+            }
+
+            Console.WriteLine("Fim");
+        }
+    }
+}
